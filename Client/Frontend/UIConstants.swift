c66aff684c3291f6c1df668e7c0325324bce07a3
Stephan Leroux <sleroux@mozilla.com>
2015-12-17 13:25:14 -0500

    Bug 1233418 - Added strings needed for login manager

--- Client/Frontend/UIConstants.swift
+++ Client/Frontend/UIConstants.swift
@@ -60,6 +60,26 @@ private struct TempStrings {
     // Bug 1186013 - Prompt for going to clipboard URL
     let goToCopiedURL = NSLocalizedString("Go to copied URL?", comment: "Pending feature; currently unused string! Prompt message shown when browser is opened with URL on the clipboard")
     let goToCopiedURLButton = NSLocalizedString("Go", comment: "Pending feature; currently unused string! Button to browse to URL on the clipboard when browser is opened")
+
+    // Bug 1233418 - Login Manager Strings
+    let loginsListTitle                 = NSLocalizedString("Logins", tableName: "LoginManager", comment: "Title for Logins List View screen")
+    let loginSearchFieldTitle           = NSLocalizedString("Search", tableName: "LoginManager", comment: "Title for the search field at the top of the Logins list screen")
+    let clearSearchAccessibilityLabel   = NSLocalizedString("Clear Search", tableName: "LoginManager", comment: "Clears the search input field and exits out of input mode")
+    let searchOverlayAccessibilityLabel = NSLocalizedString("Enter Search Mode", tableName: "LoginManager", comment: "Accessibility label for entering search mode for logins")
+    let detailUsernameRowTitle          = NSLocalizedString("username", tableName: "LoginManager", comment: "Title for username row in Login Detail View")
+    let detailPasswordRowTitle          = NSLocalizedString("password", tableName: "LoginManager", comment: "Title for password row in Login Detail View")
+    let detailWebsiteRowTitle           = NSLocalizedString("website", tableName: "LoginManager", comment: "Title for website row in Login Detail View")
+    let deleteLoginDetail               = NSLocalizedString("Delete", tableName: "LoginManager", comment: "Button in login detail screen that deletes the current login")
+    let lastLoginModified               = NSLocalizedString("Last modified %@", tableName: "LoginManager", comment: "Footer label describing when the login was last modifed")
+    let revealPassword                  = NSLocalizedString("Reveal", tableName: "LoginManager", comment: "Reveal password text selection menu item")
+    let openAndFill                     = NSLocalizedString("Open & Fill", tableName: "LoginManager", comment: "Open and Fill website text selection menu item")
+    let deselectAll                     = NSLocalizedString("Deselect All", tableName: "LoginManager", comment: "Title for deselecting all selected logins")
+    let selectAll                       = NSLocalizedString("Select All", tableName: "LoginManager", comment: "Title for selecting all logins")
+    let deletePrompt                    = NSLocalizedString("Are you sure you want to delete all your logins?", tableName: "LoginManager", comment: "Prompt message warning user about deleting all of their logins")
+    let deleteSelectedPrompt            = NSLocalizedString("Are you sure you want to delete the selected logins?", tableName: "LoginManager", comment: "Prompt message warning user about deleting the selected logins")
+    let deleteSinglePrompt              = NSLocalizedString("Are you sure you want to delete this login?", tableName: "LoginManager", comment: "Prompt message warning user about deleting a single login")
+    let deleteSyncedDevices             = NSLocalizedString("Deleted logins will be removed from all your connected devices.", tableName: "LoginManager", comment: "Prompt message warning the user that deleted logins will remove logins from all connected devices")
+    let noResultsFound                  = NSLocalizedString("No Results Found", tableName: "LoginManager", comment: "Title displayed when no results are found after searching")
 }
 
 /// Old strings that will be removed when we kill 1.0. We need to keep them around for now for l10n export.
